58a91c33beb505482f03f213d0eaf9622bf11671
knowknowledge <knowknowledge@40d08037-4d43-4175-bee4-ccfa03fdb620>
2009-10-30 05:28:35 +0000

    Simple universe cleanup. Removed most of the ships, 100 was interesting but pushed things too far. Ships are now generated near each planet based off of hardcoded locations and traffic. Changed all global functions and variables to CamelCase since underscores don't work in the console now. Created debug fuctions 'stop' and 'go' for console testing.
    git-svn-id: http://epiar.net/repo/epiar@67 40d08037-4d43-4175-bee4-ccfa03fdb620

--- Lua/scripts/universe.lua
+++ Lua/scripts/universe.lua
@@ -1,37 +1,39 @@
 
 -- Use this script for a solar system
--- This is responsible for
 
--- Generate Ships
-number_of_ships = 200
-ship_list = {}
+-- Lua Global variables
+shipList = {}  -- The ships that Lua has access to.  TODO: This list should probably be generated at each tick from a c++ call.
+frozen=0 -- Setting this to 1 causes all AI to stop 'thinking' and just drift.
 
-for s =1,number_of_ships do
-	io.write("Creating Ship ",s)
-	cur_ship = EpiarLua.Ship:new(
-		math.random(1000)-500, -- X
-		math.random(1000)-500, -- Y
-		"Terran FV-5 Frigate", -- Ship Model
-		"chase"                 -- Ship Script
-		)
-	-- io.write( cur_ship["__self"] )  -- userdata
-	-- io.write( cur_ship["__index"] ) -- table
-	--print( cur_ship.__index )
-	ship_list[s] =  cur_ship
-	x,y = EpiarLua.Ship.GetPosition(cur_ship)
-	--EpiarLua.Ship.Rotate(cur_ship,1)
+function CreateShips(number_of_ships, X, Y)
+    -- Generate Ships
+    for s =1,number_of_ships do
+        io.write("Creating Ship ",s)
+        cur_ship = EpiarLua.Ship:new(
+            math.random(1000)-500+X, -- X
+            math.random(1000)-500+Y, -- Y
+            "Terran FV-5 Frigate", -- Ship Model
+            "chase"                 -- Ship Script
+            )
+        -- io.write( cur_ship["__self"] )  -- userdata
+        -- io.write( cur_ship["__index"] ) -- table
+        --print( cur_ship.__index )
+        table.insert(shipList, cur_ship )
+        x,y = EpiarLua.Ship.GetPosition(cur_ship)
+        --EpiarLua.Ship.Rotate(cur_ship,1)
+    end
+
+    io.write("Ships Total: ", #shipList ,"\n")
 end
 
-io.write("Ships Total: ", #ship_list ,"\n")
-
-function Update ()
+function SwarmAverage()
 	-- Find the swarm center
 	avg_x,avg_y = 0,0
 	avg_angle = 0
 	avg_vector= 0
 	avg_speed = 0
-	for s =1,#ship_list do
-		cur_ship = ship_list[s]
+	for s =1,#shipList do
+		cur_ship = shipList[s]
 		x,y = EpiarLua.Ship.GetPosition(cur_ship)
 		avg_x = avg_x + x
 		avg_y = avg_y + y
@@ -42,19 +44,30 @@ function Update ()
 		avg_vector= avg_vector+ vec
 		avg_speed = avg_speed + speed
 	end
-	avg_x = avg_x / #ship_list
-	avg_y = avg_y / #ship_list
-	avg_angle = avg_angle / #ship_list
-	avg_vector = avg_vector / #ship_list
-	avg_speed = avg_speed / #ship_list
+	avg_x = avg_x / #shipList
+	avg_y = avg_y / #shipList
+	avg_angle = avg_angle / #shipList
+	avg_vector = avg_vector / #shipList
+	avg_speed = avg_speed / #shipList
+    return avg_x,avg_y,avg_angle,avg_vector,avg_speed
+end
+
+-- The C++ engine calls this function once per tick.
+-- This function is responsible for directing all of the game AI.
+function Update ()
+	if frozen>0 then
+        return 1
+    end
+
+    avg_x,avg_y,avg_angle,avg_vector,avg_speed = SwarmAverage()
 
 	-- Move towards the center
-	for s =1,number_of_ships do
-		cur_ship = ship_list[s]
+	for s =1, # shipList do
+		cur_ship = shipList[s]
 		x,y = EpiarLua.Ship.GetPosition(cur_ship)
-		dir_point = EpiarLua.Ship.directionTowards(cur_ship, avg_x, avg_y)
-		dir_aim = EpiarLua.Ship.directionTowards(cur_ship, avg_angle )
-		dir_center = EpiarLua.Ship.directionTowards(cur_ship, 0,0)
+		dir_point = EpiarLua.Ship.directionTowards(cur_ship, avg_x, avg_y) -- direction towards the center of the swarm
+		dir_aim = EpiarLua.Ship.directionTowards(cur_ship, avg_angle ) -- direction towards the swarm direction
+		dir_center = EpiarLua.Ship.directionTowards(cur_ship, 0,0) -- direction towards the center of the universe
 
 		if math.sqrt(x*x + y*y) >1000 then 
 			EpiarLua.Ship.Rotate(cur_ship, dir_center)
@@ -72,4 +85,21 @@ function Update ()
 		EpiarLua.Ship.Accelerate(cur_ship )
 	end
 end
+
+-- Functions to use from the console. ( Enter the console by hitting backtick. )
+-- TODO: Remove these functions, they are only interesting for debugging and even very useful then.
+function stop()
+    frozen=1
+end
+
+function go()
+    frozen=0
+end
+
 	
+-- Create Some ships around the planets
+-- TODO, Lua should create these ships based off of information found in the planets-default.xml
+CreateShips(3,345,215)
+CreateShips(6,-40,-135)
+CreateShips(6,4640,-735)
+
