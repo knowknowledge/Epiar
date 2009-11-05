4195fbfa60bab842055ef0fb3cd7c94f08a2d7b3
chris <chris@40d08037-4d43-4175-bee4-ccfa03fdb620>
2009-11-05 02:05:16 +0000

    Added detection code for GL_RGBA vs GL_BGRA
    git-svn-id: http://epiar.net/repo/epiar@84 40d08037-4d43-4175-bee4-ccfa03fdb620

--- Graphics/image.cpp
+++ Graphics/image.cpp
@@ -246,7 +246,10 @@ bool Image::ConvertToTexture( SDL_Surface *s ) {
  	GLenum img_format, img_type;
 	switch (s->format->BitsPerPixel) {
 		case 32:
-			img_format = GL_BGRA;
+			if(s->format->Bmask == 0x00ff0000)
+				img_format = GL_RGBA;
+			else
+				img_format = GL_BGRA;
 			img_type = GL_UNSIGNED_BYTE;
 			internal_format = GL_RGBA8;
 			break;
