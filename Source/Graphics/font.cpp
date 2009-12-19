99a9144df9f19eb000e49f4bafd896625c373479
Matt Zweig <thezweig@gmail.com>
2009-12-19 00:20:33 -0800

    Adds FreeType support as an option.
    The configure.in needs to be fixed to be more 'idiomatic'.
    It should be possible to compile when freetype is not installed.

--- Source/Graphics/font.cpp
+++ Source/Graphics/font.cpp
@@ -11,9 +11,10 @@
 #include "Graphics/afont/afont_gl.h"
 #include "includes.h"
 #include "Utilities/log.h"
+#include <FTGL/ftgl.h>
+#include "Graphics/video.h"
 
 Font::Font() {
-	font = NULL;
 	filename = NULL;
 
 	r = 1.;
@@ -21,26 +22,27 @@ Font::Font() {
 	b = 1.;
 }
 
-Font::Font( const char *filename ) {
-	font = NULL;
-	this->filename = NULL;
+void Font::SetColor( float r, float g, float b ) {
+	this->r = r;
+	this->g = g;
+	this->b = b;
+}
 
-	r = 1.;
-	g = 1.;
-	b = 1.;
+// AFONT
 
+AFont::AFont( const char *filename ) {
 	SetFont( filename );
 }
 
-Font::~Font() {
-	afont_gl_free( font );
-
+AFont::~AFont() {
+	afont_gl_free( (afontgl*)font );
+	delete (afontgl*)this->font;
 	Log::Message( "Font '%s' freed.", filename );
-
 	free( filename );
 }
 
-bool Font::SetFont( const char *filename ) {
+bool AFont::SetFont( const char *filename ) {
+	printf( "loading font '%s'...\n", filename );
 	font = afont_gl_load( filename );
 
 	if( font == NULL ) {
@@ -50,35 +52,88 @@ bool Font::SetFont( const char *filename ) {
 
 	this->filename = strdup( filename );
 
-	afont_size_text( font->orig, "A", &(this->width), &(this->height), &(this->base));
+	afont_size_text( ((afontgl*)font)->orig, "A", &(this->width), &(this->height), &(this->base));
 
 	Log::Message( "Font '%s' loaded.\n", filename );
 
 	return( true );
 }
 
-void Font::Render( int x, int y, const char *text ) {
+void AFont::Render( int x, int y, const char *text ) {
 	glEnable( GL_TEXTURE_2D );
 	glEnable(GL_BLEND);
 
 	glColor4f( r, g, b, 1. );
 	glRasterPos2i( x, y + height); // + height so that the top corner is at (x,y) like everything else.
 
-	afont_gl_render_text( font, text );
+	afont_gl_render_text( (afontgl*)font, text );
 }
 
 // Renders text centered squarely on (x,y), taking the bounding box into account
-void Font::RenderCentered( int x, int y, const char *text ) {
+void AFont::RenderCentered( int x, int y, const char *text ) {
+	glColor4f( r, g, b, 1. );
+
 	int w, h, base;
 
 	// determine size of text
-	afont_size_text( font->orig, text, &w, &h, &base );
+	afont_size_text( ((afontgl*)font)->orig, text, &w, &h, &base );
 
 	Render( x - (w / 2), y - (h / 2) , text ); // -1 because it just kinda looks better
 }
 
-void Font::SetColor( float r, float g, float b ) {
-	this->r = r;
-	this->g = g;
-	this->b = b;
+// FreeFont
+
+FreeFont::FreeFont( const char *filename ) {
+	SetFont( filename );
 }
+
+FreeFont::~FreeFont() {
+	delete (FONTRENDERTYPE*)this->font;
+	Log::Message( "Font '%s' freed.", filename );
+	free( filename );
+}
+
+bool FreeFont::SetFont( const char *filename ) {
+	printf( "loading font '%s'...\n", filename );
+	this->font = new FONTRENDERTYPE(filename);
+
+	if( font == NULL ) {
+		Log::Error( "Failed to load font '%s'.\n", filename );
+		return( false );
+	}
+
+	this->filename = strdup( filename );
+
+	font->FaceSize(12);
+
+	Log::Message( "Font '%s' loaded.\n", filename );
+
+	return( true );
+}
+
+void FreeFont::Render( int x, int y, const char *text ) {
+	glColor4f( r, g, b, 1. );
+	glPushMatrix(); // to save the current matrix
+	glScalef(1, -1, 1); 
+	
+	FTPoint pt = FTPoint( x, -y, 1);
+	((FONTRENDERTYPE*)font)->Render(text,-1,pt);
+	glPopMatrix(); // restore the previous matrix
+}
+
+// Renders text centered squarely on (x,y), taking the bounding box into account
+void FreeFont::RenderCentered( int x, int y, const char *text ) {
+	glColor4f( r, g, b, 1. );
+	glPushMatrix(); // to save the current matrix
+	glScalef(1, -1, 1); 
+
+	float llx, lly, llz;
+    float urx, ury, urz;
+
+	((FONTRENDERTYPE*)font)->BBox(text, llx, lly, llz, urx, ury, urz);
+
+	FTPoint pt = FTPoint( x + (llx - urx)/2, -(y - (lly-ury)/2), 1);
+	((FONTRENDERTYPE*)font)->Render(text,-1,pt);
+	glPopMatrix(); // restore the previous matrix
+}
+
--- Source/Graphics/font.h
+++ Source/Graphics/font.h
@@ -11,26 +11,56 @@
 #define H_FONT
 
 #include "includes.h"
+#include <FTGL/ftgl.h>
 #include "Graphics/afont/afont_gl.h"
+
+// Font Rendering technique to use
+typedef FTTextureFont FONTRENDERTYPE;
 
 class Font {
  public:
 	Font();
-	Font( const char *filename );
-	~Font();
 
-	bool SetFont( const char *filename );
-	void Render( int x, int y, const char *text );
-	void RenderCentered( int x, int y, const char *text );
+	virtual bool SetFont( const char *filename ) = 0;
+	virtual void Render( int x, int y, const char *text ) = 0;
+	virtual void RenderCentered( int x, int y, const char *text ) = 0;
 	void SetColor( float r, float g, float b );
 
-	afontgl *font;
 
  private:
-	//afontgl *font; // handle to font
 	char *filename; // filename of the loaded font
 	float r, g, b; // color of text
 	int height,width,base;
+
+	friend class AFont;
+	friend class FreeFont;
+};
+
+// Epiar specific afont style
+class AFont: public Font {
+ public:
+	AFont() { font=NULL; }
+	AFont( const char *filename );
+	~AFont();
+	bool SetFont( const char *filename );
+	void Render( int x, int y, const char *text );
+	void RenderCentered( int x, int y, const char *text );
+
+ private:
+	afontgl* font;
+};
+
+// Uses the FreeType and FTGL libraries
+class FreeFont: public Font {
+ public:
+	FreeFont() { font=NULL; }
+	FreeFont( const char *filename );
+	~FreeFont();
+	bool SetFont( const char *filename );
+	void Render( int x, int y, const char *text );
+	void RenderCentered( int x, int y, const char *text );
+ private:
+	FONTRENDERTYPE* font;
 };
 
 #endif // H_FONT
