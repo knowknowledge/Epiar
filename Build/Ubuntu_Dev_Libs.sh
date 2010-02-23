1d38204b119c4d3721fdc020387b01d9e9c72242
maoserr <maoserr@maoserr-desktop.(none)>
2010-02-22 21:37:20 -0500

    1) Added script to install Ubuntu development libraries automatically 2) Fixed liblua name in Ubuntu install file.

---
+++ Build/Ubuntu_Dev_Libs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# You also need `cmake` and `cmake-qt-gui` if you want to use cmake
+
+sudo aptitude install g++ zlib1g-dev libxml2-dev libsdl1.2-dev liblua5.1-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libftgl-dev
