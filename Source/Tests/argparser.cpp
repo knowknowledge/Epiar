d8c6f087a0ee85df4bcac4be750bae3f654aa6b2
maoserr <maoserr@gmail.com>
2010-03-21 20:43:18 -0400

    Added argument parser test.

---
+++ Source/Tests/argparser.cpp
@@ -0,0 +1,59 @@
+/**\file		argparser.cpp
+ * \author		Maoserr
+ * \date		Created: Sunday, March 21, 2010
+ * \date		Modified: Sunday, March 21, 2010
+ * \brief		Tests the command line parser.
+ */
+
+#include "includes.h"
+#include "Utilities/argparser.h"
+
+/**\brief This function tests all the available options of the ArgParser
+ * class.*/
+int test_argparser(int argc, char **argv){
+	// Create an artifical argc and argv for testing
+	int test_argc = 5;
+	char *test_argv[]={(char*)"programname.exe",
+			(char*)"-h",
+			(char*)"--help",
+			(char*)"--answer=blah",
+			(char*)"an-argument"};
+	ArgParser argpInst(test_argc,test_argv);
+
+	// Test for arguments
+	argpInst.SetOpt(SHORT,"h","Display help screen");
+	argpInst.SetOpt(LONG,"help","Display help screen");
+	argpInst.SetOpt(VALUE,"answer","Display the answer to life");
+
+	if ( argpInst.HaveShort("h"))
+		cout<<"Success: Short option."<<endl;
+	else{
+		cout<<"Failed: Short option failed to parse."<<endl;
+		return -1;
+	}
+
+	if ( argpInst.HaveLong("help") )
+		cout<<"Success: Long option."<<endl;
+	else{
+		cout<<"Failed: Long option failed to parse."<<endl;
+		return -1;
+	}
+
+	string ans=argpInst.HaveValue("answer");
+	if ( !ans.empty() )
+		cout<<"Success: Value option answer: "<<ans<<endl;
+	else{
+		cout<<"Failed: Value option failed to parse."<<endl;
+		return -1;
+	}
+
+	// Note that we don't have to use SetOpt for arguments without "-"
+	if ( argpInst.HaveArg("an-argument") )
+		cout<<"Success: Argument"<<endl;
+	else{
+		cout<<"Failed: Argument failed to parse."<<endl;
+		return -1;
+	}
+
+	return 0;
+}
---
+++ Source/Tests/argparser.h
@@ -0,0 +1,11 @@
+/**\file		argparser.h
+ * \author		Maoserr
+ * \date		Created: Sunday, March 21, 2010
+ * \date		Modified: Sunday, March 21, 2010
+ * \brief		Tests the command line parser.
+ */
+
+#ifndef __H_TESTS_ARGPARSER__
+#define __H_TESTS_ARGPARSER__
+int test_argparser(int argc, char **argv);
+#endif//__H_TESTS_ARGPARSER__
