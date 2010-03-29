7668d47cd5374679e7f3cba67a4e0e4bc41887f6
maoserr <maoserr@gmail.com>
2010-03-28 20:30:10 -0400

    Testing new UI AddChild function

--- Source/Tests/ui.cpp
+++ Source/Tests/ui.cpp
@@ -20,14 +20,14 @@ int test_ui(int argc, char **argv){
 	Tab *tab1 = static_cast<Tab*>(tabcont->AddChild(new Tab("Tab1")));
 	Tab *tab2 = static_cast<Tab*>(tabcont->AddChild(new Tab("Tab2")));
 
-	Widget *aimg = tab1->AddChild(new Picture(50,100,50,50,"Resources/Graphics/corvet.png"));
-	Widget *achk = tab1->AddChild(new Checkbox(10,120,true,"Hello"));
-	Widget *abut = tab1->AddChild(new Button(100,300,50,20,"A button"));
-	Widget *abut2 = tab1->AddChild(new Button(100,340,50,20,"A button2"));
+	tab1->AddChild(new Picture(50,100,50,50,"Resources/Graphics/corvet.png"));
+	tab1->AddChild(new Checkbox(10,120,true,"Hello"));
+	tab1->AddChild(new Button(100,300,50,20,"A button"));
+	tab1->AddChild(new Button(100,340,50,20,"A button2"));
 	
-	Widget *aslid = tab2->AddChild(new Slider(5,20,100,20,"Slider"));
-	Widget *atext = tab2->AddChild(new Textbox(5,50,100,1,"Hello","Textbox"));
-	Widget *albl = tab2->AddChild(new Label(5,80,"Hello"));
+	tab2->AddChild(new Slider(5,20,100,20,"Slider"));
+	tab2->AddChild(new Textbox(5,50,100,1,"Hello","Textbox"));
+	tab2->AddChild(new Label(5,80,"Hello"));
 	
 	while( !quit ) {
 		quit = inputs.Update();
