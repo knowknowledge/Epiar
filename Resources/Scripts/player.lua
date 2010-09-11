a34ffafabecf034ebfd69ccdcbbb528b7c317a9d
Christopher Thielen <chris@epiar.net>
2010-09-11 14:59:11 -0700

    Updated boarding dialog text to be a bit more professional.

--- Resources/Scripts/player.lua
+++ Resources/Scripts/player.lua
@@ -95,8 +95,8 @@ function boardShip()
 		-- show the boarding dialog
 		local moneyOnBoard = targettedShip:GetTotalCost() + targettedShip:GetCredits()
 		boardingDialog = UI.newWindow(100, 100, 300, 150, "Boarding Ship")
-		boardingDialog:add( UI.newLabel(50, 30, "That was easy!") )
-		boardingDialog:add( UI.newButton(50, 80, 200, 30, "Take the Monies!", string.format("doBoarding(%d)", moneyOnBoard ) ) )
+		boardingDialog:add( UI.newLabel(50, 30, "You have boarded their ship.") )
+		boardingDialog:add( UI.newButton(50, 80, 200, 30, "Steal their credits", string.format("doBoarding(%d)", moneyOnBoard ) ) )
 
 	else
 		HUD.newAlert("Cannot board target -- too far away")
