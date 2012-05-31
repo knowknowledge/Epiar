5b4f2a7f94ae9ff0caebc26755f85e57b217f5be
Christopher Thielen <cthielen@gmail.com>
2012-05-30 18:45:09 -0700

    Updated and moved OS X build scripts

--- Build/OS X/mkbundle.sh
+++ Build/OS X/mkbundle.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
+EPIAR_VERSION_FULL=`awk '$2 == "EPIAR_VERSION_FULL" {print $3}' Source/version.h | tr -d '"'`
+echo "Update the version Number in the Info.plist to $EPIAR_VERSION_FULL."
+defaults write `pwd`/Info CFBundleShortVersionString "$EPIAR_VERSION_FULL"
+defaults read `pwd`/Info
 
-# To be run from the Epiar source base directory. Generates a
-# Epiar.app directory.
-mkdir -p Epiar.app/Contents/MacOS
-cp ./Epiar Epiar.app/Contents/MacOS
-cp -r Resources Epiar.app/Contents/
-cp Build/OS\ X/Info.plist Epiar.app/Contents/
-cp Build/OS\ X/Epiar_Icon.icns Epiar.app/Contents/Resources/
-rm -f Epiar.app/Contents/Resources/Graphics/*.xcf
-rm -f Epiar.app/Contents/Resources/Definitions/saved-games.xml
+DMG_LOC=`pwd`/Epiar$EPIAR_VERSION_FULL/
+APP_LOC=$DMG_LOC/Epiar.app/
+echo "Creating Epiar.app Package at $APP_LOC"
+mkdir -p $APP_LOC/Contents/MacOS
+cp Epiar $APP_LOC/Contents/MacOS
+cp -r Resources $APP_LOC/Contents/
+cp Info.plist $APP_LOC/Contents/
+cp Resources/Icons/Epiar_Icon.icns $APP_LOC/Contents/Resources/
 
+# The Blueprint files are used for creating the Art.
+echo "Removing blueprint files."
+rm -rf $APP_LOC/Contents/Resources/Blueprints
+
+echo "Adding README and LICENSE files."
+cp AUTHORS $DMG_LOC
+cp COPYING $DMG_LOC
+cp LICENSE* $DMG_LOC
+cp README $DMG_LOC
+
+# Package Epiar into a .dmg 
+echo "Creating Epiar dmg..."
+hdiutil create Epiar$EPIAR_VERSION_FULL.dmg -srcfolder $DMG_LOC
