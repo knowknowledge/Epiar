2c951079a89a3c6f10bc3d71dce344dfec1b5fbe
Christopher Thielen <cthielen@gmail.com>
2012-05-30 19:16:16 -0700

    Enhancements to the source tarball build script

--- Build/Source/mktarball.sh
+++ Build/Source/mktarball.sh
@@ -5,6 +5,9 @@
 #
 # This is meant to be run from the project root.
 
+# Turn on Bash's extended pattern matching
+shopt -s extglob
+
 # First, clean up
 make clean
 
@@ -15,6 +18,12 @@ VERSION=$(cat Source/version.h | grep EPIAR_VERSION_FULL | cut -d'"' -f2)
 mkdir "epiar-$VERSION"
 
 # Copy everything into it - we'll filter out what we don't need later
-cp -R * "epiar-$VERSION"
-rm "epiar-$VERSION/autom4te.cache"
+cp -R !(epiar-$VERSION) epiar-$VERSION
+rm -rf "epiar-$VERSION/autom4te.cache"
+rm -rf "epiar-$VERSION/Doxygen"
+rm epiar-$VERSION/{config.log,config.status,config.h,Makefile,Makefile.in,stamp-h1,missing,depcomp,config.sub,config.guess,install-sh}
+
+# Generate the tarball and clean up our mess
+tar --bzip2 -cf epiar-$VERSION.tar.bz2 epiar-$VERSION
+rm -rf epiar-$VERSION
 
